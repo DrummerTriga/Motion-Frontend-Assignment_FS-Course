a0f7c46c8589c7d727d1c3f9dbfa320feaee0a6d
Jan Tillmann <j.tillmann@nawida.de>
2025-11-17 12:40:53 +0100

    Add 08-createHeaderComponent-JT

--- motion_teamhungry/src/elements/Tags/TagButton.jsx
+++ motion_teamhungry/src/elements/Tags/TagButton.jsx
@@ -2,9 +2,7 @@
 import xIcon from "../../assets/x-button.svg"
 
 const TagButton = ({ label }) => {
-  const onClickHandler = () => {
-    //todo define a function to delete a tag
-  }
+  const onClickHandler = () => {}
 
   return (
     <button
@@ -12,7 +10,7 @@ const TagButton = ({ label }) => {
       className="bg-zinc-100 rounded-4xl inline-flex w-fit py-2 px-4 items-center gap-3 justify-between hover:bg-zinc-200 cursor-pointer"
       onClick={onClickHandler}
     >
-      {label} <img className="h-3" src={xIcon} />
+      {label} <img className="h-3" alt="cross icon" src={xIcon} />
     </button>
   )
 }
