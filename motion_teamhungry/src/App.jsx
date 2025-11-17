fe62f9888af2d460306630fe525e19e66a788a72
Gil Santos <gilsantos.engineer@gmail.com>
2025-11-17 11:13:42 +0100

    6-pushingAuthInputFields-gs

--- motion_teamhungry/src/App.jsx
+++ motion_teamhungry/src/App.jsx
@@ -1,17 +1,11 @@
 import "./App.css"
 import axios from "axios" //todo test to import axios
-import InputFieldIcon from "./elements/Login/InputFieldIcon"
-import InputField from "./elements/Login/InputField"
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-100 border">
-        <InputFieldIcon
-          placeholder={"Email"}
-          icon={"../../../public/avatar.svg"}
-        />
-        <InputField placeholder={"Username"} label={"Username"} />
+      <div className="flex justify-center items-center h-100 bg-amber-500">
+        <h1 className="text-5xl font-bold">We are Team Hungry</h1>
       </div>
     </>
   )
