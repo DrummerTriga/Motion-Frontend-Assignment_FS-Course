30b0201b8da1ffe8ea5d9f02bebbd0f1051407a9
Jan Tillmann <j.tillmann@nawida.de>
2025-11-18 18:42:14 +0100

    Fixed pointer cursor

--- motion_teamhungry/src/components/Header/NotificationDropdown.jsx
+++ motion_teamhungry/src/components/Header/NotificationDropdown.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 
 const NotificationDropdown = () => {
    //We need the results Array from the API
-
    //JTI todo this needs to be our own ID for the loggedin user
    const ownId = 1
    const [friendRequests, setfriendRequests] = useState([
