74025c7c15636af0c0568ea60a184b00b615ab3c
rafhdr <rafaela.huonder@gmail.com>
2025-11-18 14:04:31 +0100

    registration email going to confirmation page

--- motion_teamhungry/src/ProtectedRoutes.jsx
+++ motion_teamhungry/src/ProtectedRoutes.jsx
@@ -1,26 +1,31 @@
-import { useSelector } from "react-redux"
-import { Outlet, Navigate, useLocation } from "react-router"
+import { useSelector } from 'react-redux'
+import { Outlet, Navigate, useLocation } from 'react-router'
 
+// todo - RH - need to check 1) if token in local storage (done!) and 2) if the token is still valid!!! (open)
 const ProtectedRoutes = () => {
-  const authIsLoading = useSelector((state) => state.auth.auth_is_loading)
-  const accessToken = localStorage.getItem("access_token")
-  const location = useLocation()
+   const authIsLoading = useSelector((state) => state.auth.auth_is_loading)
+   const accessToken = localStorage.getItem('access_token')
+   const location = useLocation()
 
-  console.log("Protected route check: here is the token", accessToken)
+   console.log('Protected route check: here is the token', accessToken)
 
-  if (authIsLoading) {
-    ;(console.log("loading page!!!"), (<div>Loading...</div>))
-  }
+   if (authIsLoading) {
+      ;(console.log('loading page!!!'), (<div>Loading...</div>))
+   }
 
-  if (accessToken) {
-    console.log("person is logged in")
-    return <Outlet />
-  } else {
-    console.log("person NOT logged in")
-    return (
-      <Navigate to={"/auth/signup-email"} replace state={{ from: location }} />
-    )
-  }
+   if (accessToken) {
+      console.log('person is logged in')
+      return <Outlet />
+   } else {
+      console.log('person NOT logged in')
+      return (
+         <Navigate
+            to={'/auth/signup-email'}
+            replace
+            state={{ from: location }}
+         />
+      )
+   }
 }
 
 export default ProtectedRoutes
--- motion_teamhungry/src/components/Auth/LoginForm.jsx
+++ motion_teamhungry/src/components/Auth/LoginForm.jsx
@@ -1,94 +1,86 @@
-import { Link, useNavigate } from "react-router"
-import { motion_api_no_auth } from "../../axios/axiosBase"
-import InputFieldIcon from "../../elements/Login/InputFieldIcon"
-import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { login } from "../../store/slices/authSlice"
+import { Link, useNavigate } from 'react-router'
+import { motion_api_no_auth } from '../../axios/axiosBase'
+import InputFieldIcon from '../../elements/Login/InputFieldIcon'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../store/slices/authSlice'
 
 const LoginForm = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const userData = useSelector((state) => state.auth.user_data)
+   const dispatch = useDispatch()
+   const navigate = useNavigate()
+   const userData = useSelector((state) => state.auth.user_data)
 
-  console.log("Check data from redux", userData.access)
+   console.log('data from redux', userData.access)
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [loginError, setLoginError] = useState(null)
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const [loginError, setLoginError] = useState(null)
 
-  const handleLoginSubmit = async (event) => {
-    console.log("code submitted")
-
-    event.preventDefault()
-
-    const success = await fetchingUserDataWithToken()
-
-    if (success) {
-      navigate("/profile")
-    } else {
-      setLoginError("Login failed. Please check your email and password")
-    }
-  }
-
-  // =================================================================
-  // FETCHING
-  async function fetchingUserDataWithToken() {
-    console.log("checkpoint 1", email, password)
-    try {
-      const response = await motion_api_no_auth.post("auth/token/", {
-        email,
-        password,
-      })
-      console.log("checkpoint 2 response is", response)
-      localStorage.setItem("access_token", response.data.access)
-      dispatch(login(response.data))
-      return true
-    } catch (error) {
-      console.log(error)
-      // todo - RH - add more cases to handle errors
-      if (error.response) {
-        console.log(error)
-        return false
+   // =================================================================
+   // FETCHING
+   async function handleLoginSubmit(event) {
+      event.preventDefault()
+      console.log('checkpoint 1', email, password)
+      try {
+         const response = await motion_api_no_auth.post('auth/token/', {
+            email,
+            password,
+         })
+         console.log('checkpoint 2 response is', response)
+         localStorage.setItem('access_token', response.data.access)
+         dispatch(login(response.data))
+         navigate('/profile')
+      } catch (error) {
+         console.log(error)
+         // todo - RH - add more cases to handle errors
+         if (error.response) {
+            console.log(error)
+            setLoginError('Login failed. Please check your email and password')
+         }
       }
-    }
-  }
-  // =================================================================
+   }
 
-  return (
-    <div className="flex flex-col items-center h-full w-[60%]">
-      <div className="flex w-full justify-end gap-8 pr-10 pt-10">
-        <h2>Don't have an account?</h2>
-        <button className="border">SIGN UP</button>
+   // =================================================================
+
+   return (
+      <div className="flex flex-col items-center h-full w-[60%]">
+         <div className="flex w-full justify-end gap-8 pr-10 pt-10">
+            <h2>Don't have an account?</h2>
+            <button className="border">SIGN UP</button>
+         </div>
+         <h1 className="text-[40px]">Sign In</h1>
+         <form
+            className="flex flex-col justify-between items-center h-[467px] w- border mt-auto mb-auto"
+            onSubmit={handleLoginSubmit}
+         >
+            <InputFieldIcon
+               icon="../avatar.svg"
+               placeholder="Email"
+               type="email"
+               id="email"
+               value={email}
+               handleInputChange={(e) => setEmail(e.target.value)}
+            />
+            <InputFieldIcon
+               icon={'../password.svg'}
+               placeholder={'Password'}
+               type={'password'}
+               id={'password'}
+               value={password}
+               handleInputChange={(e) => setPassword(e.target.value)}
+            />
+            {
+               <div>
+                  {loginError && <p className="text-red-500">{loginError}</p>}
+               </div>
+            }
+            <Link to="/auth/password-email">Forgot Password?</Link>
+            <button className="border" type="submit">
+               SIGN IN
+            </button>
+         </form>
       </div>
-      <h1 className="text-[40px]">Sign In</h1>
-      {<div>{loginError && <p className="text-red-500">{loginError}</p>}</div>}
-      <form
-        className="flex flex-col justify-between items-center h-[467px] w- border mt-auto mb-auto"
-        onSubmit={handleLoginSubmit}
-      >
-        <InputFieldIcon
-          icon="../avatar.svg"
-          placeholder="Email"
-          type="email"
-          id="email"
-          value={email}
-          handleInputChange={(e) => setEmail(e.target.value)}
-        />
-        <InputFieldIcon
-          icon={"../password.svg"}
-          placeholder={"Password"}
-          type={"password"}
-          id={"password"}
-          value={password}
-          handleInputChange={(e) => setPassword(e.target.value)}
-        />
-        <Link to="/auth/password-email">Forgot Password?</Link>
-        <button className="border" type="submit">
-          SIGN IN
-        </button>
-      </form>
-    </div>
-  )
+   )
 }
 
 export default LoginForm
