de9814a908f5683198139a85c494a01df0f56e40
Jan Tillmann <j.tillmann@nawida.de>
2025-11-21 14:48:15 +0100

    Fix Styling

--- motion_teamhungry/src/pages/profile/ProfileEditPage.jsx
+++ motion_teamhungry/src/pages/profile/ProfileEditPage.jsx
@@ -2,12 +2,13 @@ import EditProfile from '../../components/Profile/EditProfile'
 
 const ProfileEditPage = () => {
    return (
-      <>
-         <div className="flex flex-col justify-center items-center bg-gray-100">
-            <img src="/public/users/cloud_image.png" className=" w-full" />
+      <div className="flex flex-col  justify-center items-center bg-gray-100">
+         <img src="/public/users/cloud_image.png" className=" w-full" />
+         <div className=" -mt-24 m-15">
             <EditProfile />
          </div>
-      </>
+         <div className="bg-gray-100 min-h-80"></div>
+      </div>
    )
 }
 
